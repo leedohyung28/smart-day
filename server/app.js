cc48746eb34f4741d288d5b2f208d7f5c1bb92e8
leedohyung28 <dhleee0123@gmail.com>
2024-08-04 21:49:05 +0900

    cors issue

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 dotenv.config();
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: [process.env.CLIENT_URL, process.env.SERVER_URL], credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 
